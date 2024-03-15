a954854e540760bbcdbfbcc3c74576310ad80dba
sbstnlambert <sbstn.lambert@gmail.com>
2024-03-15 17:05:01 +0100

    (feat) Add ping server function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ServerService } from './service/server.service';
-import { Observable, catchError, map, of, startWith } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  map,
+  of,
+  startWith,
+} from 'rxjs';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { DataState } from './enum/data-state.enum';
-import { Server } from './interface/server';
+import { Status } from './enum/status.enum';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +19,20 @@ import { Server } from './interface/server';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  appState$: Observable<AppState<CustomResponse>>;
+  selectedRadio: string = 'btnradio1';
   readonly DataState = DataState;
+  readonly Status = Status;
+  appState$: Observable<AppState<CustomResponse>>;
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
+  private filterSubject = new BehaviorSubject<string>('');
+  filterStatus$ = this.filterSubject.asObservable();
 
   constructor(private serverService: ServerService) {}
 
   ngOnInit(): void {
     this.appState$ = this.serverService.servers$.pipe(
       map((response) => {
+        this.dataSubject.next(response);
         return { dataState: DataState.LOADED_STATE, appData: response };
       }),
       startWith({ dataState: DataState.LOADING_STATE }),
@@ -29,7 +42,30 @@ export class AppComponent implements OnInit {
     );
   }
 
-  selectedRadio: string = 'btnradio1';
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress).pipe(
+      map((response) => {
+        const index = this.dataSubject.value.data.servers.findIndex(
+          (server) => server.id === response.data.server.id
+        );
+        this.dataSubject.value.data.servers[index] = response.data.server;
+        this.filterSubject.next('');
+        return {
+          dataState: DataState.LOADED_STATE,
+          appData: this.dataSubject.value,
+        };
+      }),
+      startWith({
+        dataState: DataState.LOADED_STATE,
+        appData: this.dataSubject.value,
+      }),
+      catchError((error: string) => {
+        this.filterSubject.next('');
+        return of({ dataState: DataState.ERROR_STATE, error: error });
+      })
+    );
+  }
 
   updateRadioButtonStyle(id: string): void {
     this.selectedRadio = id;
